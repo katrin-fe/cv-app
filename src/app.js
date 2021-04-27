2b0548dfc158b332d182e49286d067e560a12bc2
katrin-fe <katrinfellner@gmx.net>
2021-04-27 19:51:37 +0200

    added all eventListener/links

--- src/app.js
+++ src/app.js
@@ -24,3 +24,84 @@ function showInfoWebDeveloper (event) {
 
 let webdev = document.querySelector("#front-end-developer");
 webdev.addEventListener("click", showInfoWebDeveloper);
+
+function showInfoKadertraining (event) {
+  event.preventDefault();
+  let networkText = document.querySelector("#show-information");
+  networkText.innerHTML = "Networktext t ebdeveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let kadertraining = document.querySelector("#kadertraining");
+kadertraining.addEventListener("click", showInfoKadertraining);
+
+function showInfoConsultant (event) {
+  event.preventDefault();
+  let consultantText = document.querySelector("#show-information");
+  consultantText.innerHTML = "Consultant SG t ebdeveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let consultant = document.querySelector("#consultant");
+consultant.addEventListener("click", showInfoConsultant);
+
+function showInfoStudentjob (event) {
+  event.preventDefault();
+  let studentText = document.querySelector("#show-information");
+  studentText.innerHTML = "STudent Oxalis t ebdeveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let student = document.querySelector("#studentjob");
+student.addEventListener("click", showInfoStudentjob);
+
+function showInfoDigibil (event) {
+  event.preventDefault();
+  let digibilText = document.querySelector("#show-information-edu");
+  digibilText.innerHTML = "digibil text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let digibil = document.querySelector("#digibil");
+digibil.addEventListener("click", showInfoDigibil);
+
+function showInfoCoding (event) {
+  event.preventDefault();
+  let codingText = document.querySelector("#show-information-edu");
+  codingText.innerHTML = "Coding <3 text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let coding = document.querySelector("#coding");
+coding.addEventListener("click", showInfoCoding);
+
+function showInfoMaster (event) {
+  event.preventDefault();
+  let masterText = document.querySelector("#show-information-edu");
+  masterText.innerHTML = "Master psycho text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let master = document.querySelector("#master");
+master.addEventListener("click", showInfoMaster);
+
+function showInfoSize (event) {
+  event.preventDefault();
+  let sizeText = document.querySelector("#show-information-edu");
+  sizeText.innerHTML = "Sizetext psycho text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let size = document.querySelector("#size");
+size.addEventListener("click", showInfoSize);
+
+function showInfoResil (event) {
+  event.preventDefault();
+  let resilText = document.querySelector("#show-information-edu");
+  resilText.innerHTML = "RESI psycho text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let resil = document.querySelector("#resil");
+resil.addEventListener("click", showInfoResil);
+
+function showInfoZrm (event) {
+  event.preventDefault();
+  let zrmText = document.querySelector("#show-information-edu");
+  zrmText.innerHTML = "ZRMZRM psycho text deveebdeveebdeveebdeve ebdeveebdeveloper-Text ebdeveebdeveebdeveebdeve ebdeve";
+}
+
+let zrm = document.querySelector("#zrm");
+zrm.addEventListener("click", showInfoZrm);
