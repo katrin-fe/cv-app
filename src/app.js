fe782a11df575475d398b2b1351672d32302fea3
katrin-fe <katrinfellner@gmx.net>
2021-05-04 18:14:06 +0200

    improved anchor

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function showInfoFreelancer (event) {
   event.preventDefault();
   let freelanceText = document.querySelector("#show-information");
   freelanceText.innerHTML = "In 2015 I established my own business. Since then I worked on lots of interesting projects for organizations and universities - specialised in assessments and development center, management diagnostics and trainings. In 2019 my book Moderne Personalauswahl (modern personnel selection) got published. ᐅ Katrin Fellner, Zürich";
-  window.location = "#show-information";
+  window.location = "#anchor-one";
 }
 
 let freelance = document.querySelector("#psychologist");
@@ -13,7 +13,7 @@ function showInfoKadertraining (event) {
   event.preventDefault();
   let networkText = document.querySelector("#show-information");
   networkText.innerHTML = "As a network partner I designed, organized and executed assessment-trainings for job-seeking managers. Further I gave advice on the quality of psychological diagnostics and accounted for the cooperation with universities for about six years.  ᐅ Netzwerk Kadertraining GmbH, Aarau";
-  window.location = "#show-information";
+  window.location = "#anchor-one";
 }
 
 let kadertraining = document.querySelector("#kadertraining");
@@ -24,7 +24,7 @@ function showInfoConsultant (event) {
   event.preventDefault();
   let consultantText = document.querySelector("#show-information");
   consultantText.innerHTML = "Since 2019 I take part in school development for the city of St.Gallen. Part of my role is facilitating the digitalisation, internal communication and assisting the management. ᐅ Stadt St.Gallen, Dienststelle Schule und Musik";
-  window.location = "#show-information";
+  window.location = "#anchor-one";
 }
 
 let consultant = document.querySelector("#consultant");
@@ -35,7 +35,7 @@ function showInfoStudentjob (event) {
   event.preventDefault();
   let studentText = document.querySelector("#show-information");
   studentText.innerHTML = "As a student I did a lot of adminstration, organization, customer management and writing (newsletter, advertisements, website) for a therapy center. ᐅ Praxisgemeinschaft Oxalis, Effretikon";
-  window.location = "#show-information";
+  window.location = "#anchor-one";
 }
 
 let student = document.querySelector("#studentjob");
@@ -46,7 +46,7 @@ function showInfoDigibil (event) {
   event.preventDefault();
   let digibilText = document.querySelector("#show-information-edu");
   digibilText.innerHTML = "This Certificate of Advanced Studies focuses on the digital transformation in education, innovative learning designs, e-assessments and e-portfolios, training of digital skills and adaptive learning systems. ᐅ HSG University of St.Gallen";
-  window.location = "#show-information-edu";
+  window.location = "#anchor-one";
 }
 
 let digibil = document.querySelector("#digibil");
@@ -57,7 +57,7 @@ function showInfoCoding (event) {
   event.preventDefault();
   let codingText = document.querySelector("#show-information-edu");
   codingText.innerHTML = "Next goal: learning how to code. I started my first online-course in February 2021 (ᐅ SheCodes). After a few weeks of different courses and lots of coding-weekends I bulit this cv-application. I am more than happy to continue this journey.";
-  window.location = "#show-information-edu";
+  window.location = "#anchor-two";
 }
 
 let coding = document.querySelector("#coding");
@@ -68,7 +68,7 @@ function showInfoMaster (event) {
   event.preventDefault();
   let masterText = document.querySelector("#show-information-edu");
   masterText.innerHTML = "After five years of studying human behaviour I graduated in applied psychology, focused on organizational psychology, psychology of personal development and clinical psychology. ᐅ ZHAW university of applied sciences, Zurich";
-  window.location = "#show-information-edu";
+  window.location = "#anchor-two";
 }
 
 let master = document.querySelector("#master");
@@ -79,7 +79,7 @@ function showInfoTrainer (event) {
   event.preventDefault();
   let trainerText = document.querySelector("#show-information-edu");
   trainerText.innerHTML = "Between 2014 and 2015 I learned how to attend a group through a process, help improving personal selfmanagement-skills with the Zürcher Resourcen Modell ZRM (ᐅ ISMZ, University Zurich). In addition I graduated as a reslience-trainerin ( ᐅ 2017, ResilienzForum, Berlin) and as SIZE-process coach for personnel and organizational diagnostics ( ᐅ 2017, Top im Job GmbH, Marchtrenk).";
-  window.location = "#show-information-edu";
+  window.location = "#anchor-two";
 }
 
 let trainer = document.querySelector("#trainer");
@@ -90,7 +90,7 @@ function showInfoPre (event) {
   event.preventDefault();
   let preText = document.querySelector("#show-information");
   preText.innerHTML = "That's how I started... built learning environments for children for over five years, help them believe in themselves and get the best possible starter-kit for a happy and successful life.";
-  window.location = "#show-information-edu";
+  window.location = "#anchor-two";
 }
 
 let pre = document.querySelector("#previous");
@@ -101,7 +101,7 @@ function showInfoPreEdu (event) {
   event.preventDefault();
   let preEduText = document.querySelector("#show-information-edu");
   preEduText.innerHTML = "2000 - 2005: Highschool with focus on education and childrens development. Great years with lot of learning, practice and tons of self-reflecion.";
-  window.location = "#show-information-edu";
+  wwindow.location = "#anchor-two";
 }
 
 let preEdu = document.querySelector("#previous-edu");
